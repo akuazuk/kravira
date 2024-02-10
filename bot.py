805f7dfac3d7ca9ed9d48e463fdc5ea0b797f33b
akuazuk <85361124+akuazuk@users.noreply.github.com>
2024-02-10 05:24:26 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,3 @@
-#API_TOKEN = '6802893919:AAHu7eQN_IHadnX9vJU1wudHTTloaMSYHyY'
-
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 import httpx
@@ -7,7 +5,6 @@ import asyncio
 
 API_TOKEN = '6802893919:AAHu7eQN_IHadnX9vJU1wudHTTloaMSYHyY'
 EXTERNAL_API_URL = 'https://flowiseai-railway-production-aac7.up.railway.app/api/v1/prediction/216fc9ec-2253-4769-a382-fd1171ba596c'
-
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
@@ -20,28 +17,36 @@ async def send_welcome(message: types.Message):
 @dp.message_handler()
 async def send_question_to_external_api(message: types.Message):
     question_text = message.text
+    headers = {'Content-Type': 'application/json'}
+    
     async with httpx.AsyncClient() as client:
         try:
             response = await client.post(
                 EXTERNAL_API_URL,
-                json={"question": question_text},  # Текст сообщения вставляется в JSON как значение ключа "question"
-                headers={'Content-Type': 'application/json'}
+                json={"question": question_text},
+                headers=headers
             )
-            response.raise_for_status()  # Генерирует исключение для ответов с ошибкой
+            response.raise_for_status()
             data = response.json()
+            
+            # Проверьте структуру ответа от API и адаптируйте следующую строку соответственно
             answer = data.get('answer', 'Извините, не получилось обработать ваш запрос.')
             await message.answer(answer)
+            
         except httpx.HTTPStatusError as e:
-            logging.error(f"Ошибка ответа от API: {e.response.status_code}")
+            logging.error(f"Ошибка ответа от API: {e.response.status_code} - {e.response.text}")
             await message.answer('Произошла ошибка при обработке вашего запроса API.')
+            
         except httpx.RequestError as e:
-            logging.error(f"Ошибка запроса к API: {e}")
+            logging.error(f"Ошибка запроса к API: {str(e)}")
             await message.answer('Произошла ошибка при отправке запроса к API.')
+            
+        except Exception as e:
+            logging.error(f"Неизвестная ошибка: {str(e)}")
+            await message.answer('Произошла неизвестная ошибка.')
 
 async def on_startup(dp: Dispatcher):
     await bot.delete_webhook()
-    # Здесь могут быть другие действия при запуске бота
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
-
